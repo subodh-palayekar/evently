5a8a21f3915539b5ba028025637537fc7e37e616
subodh-palayekar <subodhpalayekar651@gmail.com>
2024-04-06 14:29:17 +0530

    changes

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,55 +1,59 @@
 import CheckoutButton from '@/components/shared/CheckoutButton';
 import Collection from '@/components/shared/Collection';
-import { getEventById, getRelatedEventByCategory } from '@/lib/actions/event.action'
+import { getEventById, getRelatedEventsByCategory } from '@/lib/actions/event.action';
+
 import { formatDateTime } from '@/lib/utils';
 import { SearchParamProps } from '@/types'
 import Image from 'next/image';
-import React from 'react'
 
-const EventDetails = async ({params :{id},searchParams}:SearchParamProps) => {
+const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) => {
+  const event = await getEventById(id);
 
-    const event = await getEventById(id);
+  const relatedEvents = await getRelatedEventsByCategory({
+    categoryId: event.category._id,
+    eventId: event._id,
+    page: searchParams.page as string,
+  })
 
-    const relatedEvents = await getRelatedEventByCategory({
-      categoryId: event.category._id,
-      eventId: event._id,
-      page: searchParams.page as string,
-    })
-   
-    
-    
   return (
     <>
-    <section className='flex justify-center bg-primary-50 bg-dotted-pattern bg-contain'>
-        <div className='grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl'>
-            <Image
-                src={event.imageUrl}
-                alt='hero image'
-                width={1000}
-                height={1000}
-                className='h-full min-h-[300px] object-cover object-center'
-            />
-            <div className='flex w-full flex-col gap-8 p-5 md:p-10'>
-                <div className='flex flex-col gap-6'>
-                    <h2 className='h2-bold'>{event.title}</h2>
+    <section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-contain">
+      <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl">
+        <Image 
+          src={event.imageUrl}
+          alt="hero image"
+          width={1000}
+          height={1000}
+          className="h-full min-h-[300px] object-cover object-center"
+        />
 
-                    <div className='flex flex-col gap-3 sm:flex-row sm:items-center'>
-                        <div className='flex gap-3'>
-                            <p className='p-bold-20 bg-green-500/10 rounded-full px-5 py-2 text-green-700'>{event.isFree ? 'FREE' : `$${event.price}`}</p>
-                            <p className='p-medium-16 rounded-full bg-green-500/10 px-4 py-2.5 text-gray-500'>{event.category.name}</p>
-                        </div>
-                        <p className='p-medium-18 ml-2 mt-2 sm:mt-0'>
-                            by{' '}
-                            <span>{event.organizer.firstName} {event.organizer.lastName}</span>
-                        </p>
-                    </div>
-                </div>
-                <CheckoutButton event={event}/>
+        <div className="flex w-full flex-col gap-8 p-5 md:p-10">
+          <div className="flex flex-col gap-6">
+            <h2 className='h2-bold'>{event.title}</h2>
 
-            <div className="flex flex-col gap-5">
+            <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
+              <div className="flex gap-3">
+                <p className="p-bold-20 rounded-full bg-green-500/10 px-5 py-2 text-green-700">
+                  {event.isFree ? 'FREE' : `$${event.price}`}
+                </p>
+                <p className="p-medium-16 rounded-full bg-grey-500/10 px-4 py-2.5 text-grey-500">
+                  {event.category.name}
+                </p>
+              </div>
+
+              <p className="p-medium-18 ml-2 mt-2 sm:mt-0">
+                by{' '}
+                <span className="text-primary-500">{event.organizer.firstName} {event.organizer.lastName}</span>
+              </p>
+            </div>
+          </div>
+
+          <CheckoutButton event={event} />
+
+          <div className="flex flex-col gap-5">
             <div className='flex gap-2 md:gap-3'>
               <Image src="/assets/icons/calendar.svg" alt="calendar" width={32} height={32} />
-              <div className="p-medium-16 lg:p-regular-20 flex xs:gap-2   flex-wrap items-center">
+              <div className="p-medium-16 lg:p-regular-20 flex flex-wrap items-center">
                 <p>
                   {formatDateTime(event.startDateTime).dateOnly} - {' '}
                   {formatDateTime(event.startDateTime).timeOnly}
@@ -76,7 +80,7 @@ const EventDetails = async ({params :{id},searchParams}:SearchParamProps) => {
       </div>
     </section>
 
-    {/* event with the same category */}
+    {/* EVENTS with the same category */}
     <section className="wrapper my-8 flex flex-col gap-8 md:gap-12">
       <h2 className="h2-bold">Related Events</h2>
 
