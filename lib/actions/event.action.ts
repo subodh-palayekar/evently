c0c7fdf02f0ee80ec7dba2ebce0810e7fab512a0
subodh-palayekar <subodhpalayekar651@gmail.com>
2024-04-07 16:46:43 +0530

    stripe usd to inr

--- lib/actions/event.action.ts
+++ lib/actions/event.action.ts
@@ -10,7 +10,7 @@ import { handleError } from '@/lib/utils'
 
 import {
   CreateEventParams,
-  UpdateEventParams,
+  UpdateEventParams, 
   DeleteEventParams,
   GetAllEventsParams,
   GetEventsByUserParams,
@@ -98,14 +98,14 @@ export async function deleteEvent({ eventId, path }: DeleteEventParams) {
 export async function getAllEvents({ query, limit = 6, page, category }: GetAllEventsParams) {
   try {
     await connectToDatabase()
-
     const titleCondition = query ? { title: { $regex: query, $options: 'i' } } : {}
-    const categoryCondition = category ? await getCategoryByName(category) : null
+    const categoryCondition = category ? await getCategoryByName(category) : null;
     const conditions = {
       $and: [titleCondition, categoryCondition ? { category: categoryCondition._id } : {}],
     }
 
     const skipAmount = (Number(page) - 1) * limit
+    
     const eventsQuery = Event.find(conditions)
       .sort({ createdAt: 'desc' })
       .skip(skipAmount)
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -10,10 +10,7 @@ import { handleError } from '@/lib/utils'
 
 import { CreateUserParams, UpdateUserParams } from '@/types'
 
-export async function createUser(user: CreateUserParams) {
-
-    console.log("user created action");
-    
+export async function createUser(user: CreateUserParams) {    
   try {
     await connectToDatabase()
 
